8284cfd12f3a19c62699c3c1540432d82b191dc3
henrikny <henriknyhus@gmail.com>
2017-12-28 14:00:49 +0100

    Edit

--- coffee_db.R
+++ coffee_db.R
@@ -6,12 +6,13 @@ library(lubridate)
 # setwd("/Users/henriknyhus/Dropbox/Git/coffee")
 # setwd("/srv/shiny-server/coffee")
 
-db_path <- "coffee.sqlite"
+# db_path <- "../coffee-db/coffee.sqlite"
+# db_path <- "coffee.sqlite"
 
 ###################################################
 ## CREATE NEW
 ###################################################
-## create empty database
+## connect to existing / create empty database
 coffee_db <- dbConnect(RSQLite::SQLite(), db_path)
 
 ## drop old tables if coffee_db already exists
@@ -58,23 +59,24 @@ p <- dbGetQuery(conn = coffee_db,
                 statement = "SELECT rowid, * FROM persons") %>% as_tibble()
 d <- dbGetQuery(conn = coffee_db, 
                 statement = "SELECT rowid, * FROM actions") %>% as_tibble()
-d
-p
-d %>% mutate(name_id = as.integer(name_id)) %>% left_join(p, by = c("name_id" = "rowid")) %>% View()
+
+d %>% 
+  mutate(name_id = as.integer(name_id)) %>% 
+  left_join(p, by = c("name_id" = "rowid")) %>% 
+  View()
 
 
 ###################################################
 ## MANUAL BACKFILLING
 ###################################################
-## either manually enter names or use your current persons-table as base
-p$name
 data_in <- tibble(name = p$name, 
-                  time = "2017-12-09 00:00:00", 
-                  amount = c(2, 4, 5, 7, 1, 2, 34, 2, 2, 3, 4, 8, 0, 0, 0, 1, 2, 4, 2, 0, 3, 6, 0))
+                  time = "2017-12-31 00:00:00", 
+                  amount = rep(0, nrow(p)))
+
 
 write_coffee_manual <- function(data_in){
   data_in <- data_in %>% filter(amount != 0)
-
+  
   if(any(!data_in$name %in% p$name)){
     stop("Name not recognized")
   }
@@ -84,23 +86,43 @@ write_coffee_manual <- function(data_in){
   data_in <- data_in[new_index,]
   data_in <- data_in %>% 
     select(-amount, -active) %>% 
-    mutate(action = "Single Pot")
-   
+    mutate(action = "Single pot")
+  
   rs <- dbSendStatement(conn = coffee_db, 
                         statement = "INSERT INTO actions (time, name_id, active, action, backfilled) VALUES (:time, :name_id, :active, :action, :backfilled)")
   dbBind(rs, param = list(time = str_c(data_in$time), 
                           name_id = data_in$rowid,
                           active = rep(1, nrow(data_in)),
                           action = data_in$action,
-                          backfilled = rep(0, nrow(data_in))))
+                          backfilled = rep(1, nrow(data_in))))
 }
 
 
+###################################################
+## EDIT DATA
+###################################################
+dbExecute(conn = coffee_db, 
+          statement = "UPDATE actions SET action = 'Single pot' WHERE action = 'Single Pot'")
+
+
+###################################################
+## SUMMARY STATISTICS
+###################################################
+d <- tbl(coffee_db, "actions") %>% 
+  as_tibble() %>% 
+  filter(active == 1) %>% 
+  filter(action %in% c("Single pot", "Double pot")) %>% 
+  mutate("pots" = if_else(action == "Single pot", 1, 2)) %>% 
+  mutate(name_id = as.integer(name_id)) %>% 
+  left_join(p, by = c("name_id" = "rowid")) %>% 
+  group_by(name) %>% 
+  summarize(pots = sum(pots)) %>% 
+  arrange(desc(pots)) %>% 
+  right_join(p)
+d
 
 
 ###################################################
 ## DISCONNECT
 ###################################################
 dbDisconnect(coffee_db)
-
-
