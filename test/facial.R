8b919d7675e65e9d7aad7ee937ce9a9251f42fb0
henrikny <henriknyhus@gmail.com>
2018-01-10 16:39:40 +0100

    Edit

--- test/facial.R
+++ test/facial.R
@@ -5,8 +5,11 @@ devtools::install_github("swarm-lab/Rvision")
 
 
 
+library(ROpenCVLite)
+library(Rvision)
 
 my_stream <- stream(0)   # 0 will start your default webcam in general. 
+my_stream
 my_selfie <- readNext(my_stream)
 plot(my_selfie)
 release(my_stream)
